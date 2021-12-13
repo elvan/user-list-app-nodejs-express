d2a5484a668418622d10a1eddf40d6178c94fff1
Elvan H <elvanhd@gmail.com>
2021-12-13 20:13:36 +0700

    displaying current time at a specific route

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 function handleRequest(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  if (req.url === '/current-time') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<h1>${new Date().toISOString()}</h1>`);
+  } else if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+  }
 }
 
 const server = http.createServer(handleRequest);
